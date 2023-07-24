b9ddea226d9e289201c856ea760d857088e9f467
Raghvendra Mishra <raghvendramishra2002@gmail.com>
2023-07-25 00:39:14 +0530

    Working Server setup - Data Received

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,15 @@
 const app = require('./app');
 
 const port = 3000;
-
 app.get('/', (req, res) => {
     res.send('Hello World');
 })
+
+app.post('/post', (req, res) => {
+    const data = req.body;
+    console.log(data);
+    res.send('WOrked')
+});
 
 app.listen(port, () => {
     console.log('Server listening on Port http://localhost:' + port);
