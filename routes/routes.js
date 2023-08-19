eab0d36c5736e89ccf24f2770d835f6f78f314ca
Raghvendra Mishra <raghvendramishra2002@gmail.com>
2023-08-19 15:17:07 +0530

    Homepage add

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,9 @@
 const router = require('express').Router()
 const UserController = require('../controller/test_controller')
 
+router.get('/', (req, res, next) => {
+    res.send('Hello')
+})
 router.post('/post', UserController.register)
 router.post('/login', UserController.login)   //SHA256 encrypted password passed from the front-end to the backend
 
